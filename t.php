77ace1a7ece9d8ae2b65e310bb689ee7da7c340e
lk1ngaa7 <lk1ngaa7@gmail.com>
2017-10-02 15:35:07 +0800

    first init

---
+++ t.php
@@ -0,0 +1,33 @@
+<?php
+
+// Create the image
+for($i=0;$i<=9;$i++){
+	for($k=0;$k<=9;$k++){
+		$im = imagecreatetruecolor(40, 60);
+		// Create some colors
+		$white = imagecolorallocate($im, 255, 255, 255);
+		$grey = imagecolorallocate($im, 128, 128, 128);
+		$black = imagecolorallocate($im, 0, 0, 0);
+		imagefilledrectangle($im, 0, 0, 40 , 60, $white);
+		// The text to draw
+		$text = ''.$i;
+		// Replace path by your own font path
+		$font = 'DejaVuSans.ttf';
+		// Add some shadow to the text
+		//imagettftext($im, 20, 0, 11, 21, $grey, $font, $text);
+		// Add the text
+		$angle = intval(rand(2 , 15));
+		
+		if($angle%2){
+			$angle = 0 - $angle;
+		}
+		$x = rand(1 , 5);
+		$y = rand(40 , 50);
+		imagettftext($im, 38 , $angle , 3 , 45, $black, $font, $text);
+		// Using imagepng() results in clearer text compared with imagejpeg()
+		imagepng($im , 'data/test/'.$i.'_'.$k.'.png');
+		imagedestroy($im);
+	}
+}
+?>
+
